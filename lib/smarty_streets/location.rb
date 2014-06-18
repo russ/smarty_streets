56dd69b3cc2b2946ada17db87b16fae8753ba513
alexpeachey <alex.peachey@gmail.com>
2014-06-18 10:37:55 -0700

    Extract Analysis from results

--- lib/smarty_streets/location.rb
+++ lib/smarty_streets/location.rb
@@ -3,6 +3,6 @@ module SmartyStreets
     attr_accessor :street, :street2, :secondary
     attr_accessor :city, :state, :zip_code
     attr_accessor :last_line, :addressee, :urbanization
-    attr_accessor :delivery_point_barcode, :components, :meta_data
+    attr_accessor :delivery_point_barcode, :components, :meta_data, :analysis
   end
 end
